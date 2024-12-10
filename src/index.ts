8bc054f8dcb333ec0afc44665f3fcb8de19e341c
Juan C Pablo <jcpablo10gmail.com>
2024-12-10 16:25:43 -0600

    feat: update package configuration and add entry point for library

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const Button = () => { import('./components/Button/Button.vue')}
+const SInput = () => { import('./components/SInput/SInput.vue')}
+import './style.css';
+
+export default {
+  install: (app: any, options: any) => {
+    app.component('Button', Button);
+    app.component('SInput', SInput);
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url';
+import { resolve } from 'path';
 
 // https://vite.dev/config/~
 export default defineConfig({
-  base: './',
+  base: '',
   plugins: [vue()],
   resolve: {
     alias: {
-      '~/': fileURLToPath(new URL('./src/', import.meta.url))
-    }
-  }
+      '~/': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-components-library',
+      fileName: (format) => `vue-components-library.${format}.js`,
+    },    
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        inlineDynamicImports: true,
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  
 })
